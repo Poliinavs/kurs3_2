6b450b592265416129a04c2b98b75406d5e23f3f
Poliinavs <95181222+Poliinavs@users.noreply.github.com>
2024-06-18 12:51:32 +0300

    Update zd1Token.js

--- PSKP/lab10/zd1Token.js
+++ PSKP/lab10/zd1Token.js
@@ -3,8 +3,7 @@ import { createClient } from 'webdav';
 const client = createClient(
     "https://webdav.yandex.ru/",
     {
-        username: "paulinaavsyukevitch",
-        password: "qwerasdf1234e"
+        
     }
 );
 
